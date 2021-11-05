649e249206993255de03d5bd8712c2345a75c242
Israel <israelpereira55@gmail.com>
2021-11-05 18:43:12 -0300

    added dop intra depot

--- vrp/local_search.py
+++ vrp/local_search.py
@@ -1,7 +1,7 @@
 import numpy as np
 from matplotlib import pyplot
 
-from vrp import common
+from vrp import common, plot
 
 # Just used to react like a goto on 2-opt
 class GotImprovementException(Exception):
@@ -471,9 +471,99 @@ def two_opt_vrptw_best_improvement(vrptw_solution):
 
 
 def two_opt_mdvrptw(mdvrptw_solution):
-
     for vrptw_solution in mdvrptw_solution.vrptw_solutions:
         two_opt_vrptw_first_improvement(vrptw_solution)
 
     return mdvrptw_solution
 
+
+def drop_one_point_intra_depot_vrptw(vrptw_solution):
+    vrptw = vrptw_solution.vrptw
+    number_of_clients = vrptw_solution.number_of_vertices -1
+
+    #vrptw_solution.print_solution()
+    best_pj = -1,-1
+    got_improvement = False
+    for routei_index in range(len(vrptw_solution.routes)): 
+    #for routei_index,routei in enumerate(vrptw_solution.routes): 
+        cost = vrptw_solution.travel_distance
+        routei = vrptw_solution.routes[routei_index] #enumerate nao é tao bom pq tem q atualizar
+        #for i in range(1, len(routei)-1): # (-2) take out depot on first and last position
+        i = 1
+        while i < len(routei)-1:
+            ci = routei[i]
+            c_i_less1 = routei[i-1]
+            c_i_plus1 = routei[i+1]
+
+            for routej_index in range(routei_index+1, len(vrptw_solution.routes)):
+                routej = vrptw_solution.routes[routej_index]
+                
+                for j in range(1, len(vrptw_solution.routes[routej_index])-1):
+                    cj = routej[j]
+                    c_j_less1 = routej[j-1]
+                    #c_j_plus1 = routej[j+1] #after inserting ci, cj become the new cj+1
+
+                    #print(ci, cj, f"({i},{routei_index}) x ({j}, {routej_index})")
+                    if not common.is_insertion_viable_by_demand(vrptw_solution, ci, routej_index):
+                        continue                        
+
+                    if not common.is_insertion_viable_by_time_windows(vrptw_solution, ci, j, routej_index):
+                        continue
+
+                    new_cost = cost \
+                               - vrptw.distances[c_i_less1][ci] - vrptw.distances[c_i_plus1][ci] \
+                               + vrptw.distances[c_i_less1][c_i_plus1] \
+                               \
+                               + vrptw.distances[c_j_less1][ci] + vrptw.distances[cj][ci] \
+                               - vrptw.distances[c_j_less1][cj]
+
+                    if new_cost < cost:
+                        cost = new_cost
+                        best_pj = routej_index, j
+                        got_improvement = True
+
+
+            if got_improvement:
+                best_route_index, best_index = best_pj
+                vrptw_solution.remove_client(i, routei_index)
+                vrptw_solution.insert_client(ci, best_index, best_route_index)
+                got_improvement = False
+
+                '''
+                if round(vrptw_solution.travel_distance,2) != round(vrptw_solution.calculate_cost(), 2):
+                    print(vrptw_solution.travel_distance, vrptw_solution.calculate_cost())
+                    print("deu ruim")
+                    exit(1)
+                '''
+            i+=1
+            #print('-')
+
+
+
+def drop_one_point_intra_depot_mdvrptw(mdvrptw_solution):
+    for vrptw_solution in mdvrptw_solution.vrptw_solutions:
+        drop_one_point_intra_depot_vrptw(vrptw_solution)
+
+    return mdvrptw_solution
+
+
+def local_search(mdvrptw_solution, print_solution=False):
+    got_improvement = True
+
+    while got_improvement:
+        got_improvement= False
+        cost = mdvrptw_solution.get_travel_distance()
+
+        two_opt_mdvrptw(mdvrptw_solution)
+        if print_solution and mdvrptw_solution.get_travel_distance() < cost:
+            mdvrptw_solution.print_solution()
+            plot.plot_mdvrptw_solution(mdvrptw_solution)
+
+        drop_one_point_intra_depot_mdvrptw(mdvrptw_solution)
+        if print_solution and mdvrptw_solution.get_travel_distance() < cost:
+            mdvrptw_solution.print_solution()
+            plot.plot_mdvrptw_solution(mdvrptw_solution)
+
+
+        if mdvrptw_solution.get_travel_distance() < cost:
+            got_improvement= True
