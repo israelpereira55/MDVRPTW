dfb92e315c09361de9ec5e990c3d5d5b943cdf80
Israel <israelpereira55@gmail.com>
2021-11-15 17:44:20 -0300

    changed variable names

--- vrp/depot.py
+++ vrp/depot.py
@@ -9,7 +9,8 @@ class Depot:
     index : int             # can be 1 to number of depots
     customer_id : int       # the customer number from the instance
     route_max_time : int
-    vehicle_max_load : int
+    vehicle_capacity : int
+    number_of_vehicles : int
 
     x : float               # 2D coordinates 
     y : float
@@ -17,10 +18,11 @@ class Depot:
     ready_time : float      # earliest time of service
     due_date   : float      # latest time of service
 
-    def __init__(self, index, route_max_time, vehicle_max_load):
+    def __init__(self, index, route_max_time, vehicle_capacity, number_of_vehicles):
         self.index = index
         self.route_max_time = route_max_time
-        self.vehicle_max_load = vehicle_max_load
+        self.vehicle_capacity = vehicle_capacity
+        self.number_of_vehicles = number_of_vehicles
 
         self.customer_id = 0
         self.x = self.y = self.service_time = self.ready_time = self.due_date = 0.0
