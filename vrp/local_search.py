7378deb0b424ae0a9e876ac7323d92b179da8b5f
Israel <israelpereira55@gmail.com>
2021-11-15 17:44:45 -0300

    2opt with TW check fixes

--- vrp/local_search.py
+++ vrp/local_search.py
@@ -3,12 +3,12 @@ from matplotlib import pyplot
 
 from vrp import common, plot
 
-# Just used to react like a goto on 2-opt
+# Just used to react like a goto on 2-swap
 class GotImprovementException(Exception):
     pass
 
 
-def two_opt_vrptw_hibrid_first_improvement(vrptw_solution):
+def two_swap_vrptw_hibrid_first_improvement(vrptw_solution):
     vrptw = vrptw_solution.vrptw
     number_of_clients = vrptw_solution.number_of_vertices -1
 
@@ -101,7 +101,7 @@ def two_opt_vrptw_hibrid_first_improvement(vrptw_solution):
         except GotImprovementException:
             got_improvement = True
 
-def two_opt_vrptw_hibrid_best_improvement(vrptw_solution):
+def two_swap_vrptw_hibrid_best_improvement(vrptw_solution):
     vrptw = vrptw_solution.vrptw
     number_of_clients = vrptw_solution.number_of_vertices -1
 
@@ -193,7 +193,7 @@ def two_opt_vrptw_hibrid_best_improvement(vrptw_solution):
     return
 
 
-def two_opt_vrptw_best_improvement_old(vrptw_solution):
+def two_swap_vrptw_best_improvement_old(vrptw_solution):
     vrptw = vrptw_solution.vrptw
     number_of_clients = vrptw_solution.number_of_vertices -1
 
@@ -303,10 +303,9 @@ def two_opt_vrptw_best_improvement_old(vrptw_solution):
         except GotImprovementException:
             got_improvement = True
 
-def two_opt_vrptw_first_improvement(vrptw_solution):
+def two_swap_vrptw_first_improvement(vrptw_solution):
     vrptw = vrptw_solution.vrptw
     number_of_clients = vrptw_solution.number_of_vertices -1
-
     #vrptw_solution.print_solution()
 
     got_improvement = True
@@ -383,7 +382,7 @@ def two_opt_vrptw_first_improvement(vrptw_solution):
             got_improvement = True
 
 
-def two_opt_vrptw_best_improvement(vrptw_solution):
+def two_swap_vrptw_best_improvement(vrptw_solution):
     vrptw = vrptw_solution.vrptw
     number_of_clients = vrptw_solution.number_of_vertices -1
 
@@ -470,9 +469,9 @@ def two_opt_vrptw_best_improvement(vrptw_solution):
             pass
 
 
-def two_opt_mdvrptw(mdvrptw_solution):
+def two_swap_mdvrptw(mdvrptw_solution):
     for vrptw_solution in mdvrptw_solution.vrptw_solutions:
-        two_opt_vrptw_first_improvement(vrptw_solution)
+        two_swap_vrptw_first_improvement(vrptw_solution)
 
     return mdvrptw_solution
 
@@ -547,18 +546,199 @@ def drop_one_point_intra_depot_mdvrptw(mdvrptw_solution):
     return mdvrptw_solution
 
 
+#TODO: more efficient with numpy vectors.
+def drop_one_point_inter_depot_mdvrptw(mdvrptw_solution):
+    mdvrptw = mdvrptw_solution.mdvrptw
+
+    #Defining the 'clients_current_depot', which tells for each client, the depot it's related to.
+    clients_current_depot = [-1] * (mdvrptw.number_of_clients+1)
+    for cluster in mdvrptw_solution.clustered_clients:
+        depot_index = cluster[0]
+        for i in range(1, len(cluster)):
+            client = cluster[i]
+            clients_current_depot[client] = depot_index
+                
+    global_demands = []
+    max_demands = []
+    for vrptw_subproblem in mdvrptw_solution.vrptw_subproblems:
+        global_demands.append(vrptw_subproblem.global_demand)
+        max_demands.append(vrptw_subproblem.vehicle_capacity * vrptw_subproblem.number_of_vehicles)
+
+    clients_closest_depot = []
+    #for _ in range(mdvrptw.number_of_clients):
+    #    clients_closest_depot.append([])
+
+    #for ci in range(1, mdvrptw.number_of_clients+1):
+    #    depot_distances = 
+    #    for depot in mdvrptw.depots:
+    #        if 
+
+    '''
+    client_depots = [-1] * mdvrptw.number_of_clients
+    for ci in range(1, mdvrptw.number_of_clients+1):
+        print(ci, mdvrptw.number_of_clients)
+    '''
+    print(clients_current_depot)
+    exit(1)
+
+
+# 2opt-intra
+# credits: https://stackoverflow.com/questions/53275314/2-opt-algorithm-to-solve-the-travelling-salesman-problem-in-python
+# Sadly Fradge didn't receive the best answer, but should!
+#
+# Edges
+#   Before swap: c1-c2, c3-c4 
+#   After swap:  c1-c3, c2-c4
+def cost_swap(distances, c1, c2, c3, c4):
+    return distances[c1][c3] + distances[c2][c4] - distances[c1][c2] - distances[c3][c4]
+
+def two_opt_intra_route_old(vrptw_solution, route_index, steepest_descent=True):
+    vrptw = vrptw_solution.vrptw
+    route = vrptw_solution.routes[route_index]
+    distances = vrptw.distances
+    best = route
+    #print("Started", route)
+
+    got_improvement = True
+    while got_improvement:
+        #print("Start of 2opt")
+        got_improvement = False
+
+        try:
+            #Disclaimer: as my routes are [0, 1, ..., n, 0], I have removed the last element on the iteration
+            # If your case is like [0,1,...,n], it should be i=1 to len(route)-2; j=i+2 to len(route)
+            for i in range(1, len(route)-3):
+                for j in range(i+2, len(route) -1):
+                    cost_update = cost_swap(distances, best[i-1], best[i], best[j-1], best[j])
+                    if cost_update < 0:
+
+                        new_route = best[:]
+                        new_route[i:j] = best[j-1:i-1:-1]
+                        if not common.is_feasible_by_time_windows(new_route, vrptw): #TODO, define start to save time
+                            continue
+
+                        best = new_route
+                        vrptw_solution.travel_distance += cost_update
+                        got_improvement = True
+                        #print("improved", best)
+
+                        if steepest_descent: 
+                            raise GotImprovementException
+
+                #if not steepest_descent and got_improvement:
+                #    #coisa aqui
+                #    pass
+
+            route = best
+        except GotImprovementException:
+            pass
+        #exit(1)
+
+    #print("Last", route)
+    return best
+
+def get_cost(route, distances):
+    sum_dist=0
+    ci = route[0]
+    for j in range(1, len(route)):
+        cj = route[j]
+        sum_dist+= distances[ci][cj]
+
+    return sum_dist
+
+
+
+def two_opt_intra_route(vrptw_solution, route_index, steepest_descent=True):
+    vrptw = vrptw_solution.vrptw
+    route = vrptw_solution.routes[route_index]
+    distances = vrptw.distances
+    best = route
+
+    cost = get_cost(route, vrptw.distances)
+
+    got_improvement = True
+    while got_improvement:
+        got_improvement = False
+
+        try:
+            #Disclaimer: as my routes are [0, 1, ..., n, 0], I have removed the last element on the iteration
+            # If your case is like [0,1,...,n], it should be i=1 to len(route)-2; j=i+2 to len(route)
+            for i in range(1, len(route)-3):
+                for j in range(i+2, len(route) -1):
+                        new_route = best[:]
+                        new_route[i:j] = best[j-1:i-1:-1]
+
+                        new_cost = get_cost(new_route, vrptw.distances)
+                        if new_cost < cost:
+                            if not common.is_feasible_by_time_windows(new_route, vrptw): #TODO, define start to save time
+                                continue
+
+                            cost = new_cost
+                            best = new_route
+                            vrptw_solution.travel_distance = vrptw_solution.travel_distance - (cost - new_cost)
+                            got_improvement = True
+
+                            if steepest_descent: 
+                                raise GotImprovementException
+
+                #if not steepest_descent and got_improvement:
+                #    #coisa aqui
+                #    pass
+
+            route = best
+        except GotImprovementException:
+            pass
+
+    return best
+
+def two_opt_intra_route_hibrid(route, distances):
+    got_improvement = True
+    while got_improvement:
+        got_improvement = False
+
+        #Disclaimer: as my routes are [0, 1, ..., n, 0], I have removed the last element on the iteration
+        # If your case is like [0,1,...,n], it should be i=1 to len(route)-2; j=i+2 to len(route)
+        for i in range(1, len(route)-3):
+            for j in range(i+2, len(route) -1):
+                if cost_swap(distances, route[i-1], route[i], route[j-1], route[j]) < 0:
+                    route[i:j] = route[j-1:i-1:-1]
+                    got_improvement = True
+
+    return route
+
+def two_opt_intra_route_mdvrptw(mdvrptw_solution):
+    for vrptw_solution in mdvrptw_solution.vrptw_solutions:
+        for route_index in range(len(vrptw_solution.routes)):
+            #cost = vrptw_solution.travel_distance
+            two_opt_intra_route(vrptw_solution, route_index, steepest_descent=True)
+
+            '''
+            if vrptw_solution.travel_distance < cost:
+                mdvrptw_solution.print_solution()
+                print(mdvrptw_solution.get_travel_distance())
+                plot.plot_mdvrptw_solution(mdvrptw_solution)
+            '''
+
+
 def local_search(mdvrptw_solution, print_solution=False):
     got_improvement = True
-
     while got_improvement:
         got_improvement= False
         cost = mdvrptw_solution.get_travel_distance()
 
-        two_opt_mdvrptw(mdvrptw_solution)
+
+        two_opt_intra_route_mdvrptw(mdvrptw_solution)
         if print_solution and mdvrptw_solution.get_travel_distance() < cost:
             mdvrptw_solution.print_solution()
             plot.plot_mdvrptw_solution(mdvrptw_solution)
 
+
+        two_swap_mdvrptw(mdvrptw_solution)
+        if print_solution and mdvrptw_solution.get_travel_distance() < cost:
+            mdvrptw_solution.print_solution()
+            plot.plot_mdvrptw_solution(mdvrptw_solution)
+
+        
         drop_one_point_intra_depot_mdvrptw(mdvrptw_solution)
         if print_solution and mdvrptw_solution.get_travel_distance() < cost:
             mdvrptw_solution.print_solution()
@@ -566,4 +746,5 @@ def local_search(mdvrptw_solution, print_solution=False):
 
 
         if mdvrptw_solution.get_travel_distance() < cost:
-            got_improvement= True
+            got_improvement= True 
+
