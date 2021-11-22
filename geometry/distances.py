902f9ad65a2e87ea951ee69230f5c8dfde8982fe
Israel <israelpereira55@gmail.com>
2021-11-22 19:21:49 -0300

    numpy euclidian distances

--- geometry/distances.py
+++ geometry/distances.py
@@ -1,14 +1,21 @@
 import math
 import numpy as np
 
+def euclidian_distance(p1, p2):
+    diff = p1[0] - p2[0], p1[1] - p2[1]
+    return np.linalg.norm(diff)
+
 def calculate_distance_matrix(coordinates):
     number_of_lines = len(coordinates)
     distances = np.zeros((number_of_lines, number_of_lines))
 
     for i in range(number_of_lines):
-        x1,y1 = coordinates[i]
+        #p1 = coordinates[i]
+        p1 = coordinates[i]
         for j in range(i, number_of_lines):
-            x2,y2 = coordinates[j]
-            distances[i][j] = distances[j][i] = math.sqrt( (x1-x2)**2 + (y1-y2)**2 )
+            #p2 = coordinates[j]
+            #distances[i][j] = distances[j][i] = euclidian_distance(p1, p2)
+            p2 = coordinates[j]
+            distances[i][j] = distances[j][i] = euclidian_distance(p1, p2)
 
     return distances
