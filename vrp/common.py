ff36431c04b6e3bd93af7ac6463d1e7ac7048038
Israel <israelpereira55@gmail.com>
2021-11-22 19:22:42 -0300

    added dop next depot functions and fixes

--- vrp/common.py
+++ vrp/common.py
@@ -17,6 +17,24 @@ def calculate_starting_time(vrptw, ci, bi, cj): #bj-*********************
     return max(ej, bi+si+tij)
 
 
+def calculate_starting_time_mdvrptw(mdvrptw, ci, bi, cj): #bj-*********************
+    if cj > mdvrptw.number_of_clients:
+        depot_index = cj - mdvrptw.number_of_clients -1
+        depot = mdvrptw.depots[depot_index]
+        ej = depot.ready_time
+    else:
+        ej = mdvrptw.time_windows[cj][0]
+
+    if ci > mdvrptw.number_of_clients:
+        depot_index = ci - mdvrptw.number_of_clients -1
+        depot = mdvrptw.depots[depot_index]
+        si = depot.service_time
+    else:
+        si = mdvrptw.services[ci]
+
+    tij = mdvrptw.travel_times[ci][cj]
+    return max(ej, bi+si+tij)
+
 
 # ================= FEASBILITY CHECK =================
 
@@ -86,6 +104,47 @@ def is_insertion_viable_by_time_windows(vrptw_solution, city, u, route_index, ge
         return True, [ci, bi, cj, bj, city, bu, bju]
     else:
         return True
+
+
+# Function: is_insertion_viable_by_time_windows_mdvrptw
+#
+#   * Descrption: 
+#       - Check time windows viability to insert city cm, to position (routeu_index, u), increasing the route size.
+#       - All cities which perform mdvrptw actions need to have their indexes for the mdvrptw problem
+#         Otherwise, if they are performing vrptw actions, they need to have their indexes from that vrptw subproblem.
+#
+#   * Restrictions:
+#       - city should not be on route already.
+#       - u can not be 0 or len(routeu) (Because it is the depot index)
+#       - city should be the real index from mdvrptw problem
+#
+def is_insertion_viable_by_time_windows_mdvrptw(mdvrptw_solution, problem_index, city, route_index, u):
+    mdvrptw = mdvrptw_solution.mdvrptw
+    vrptw_solution = mdvrptw_solution.vrptw_solutions[problem_index]
+    vrptw = vrptw_solution.vrptw
+    route = vrptw_solution.routes[route_index]
+    route_starting_times = vrptw_solution.get_route_starting_times(route_index)
+
+
+    ci = int(route[u-1]) #left neighbour of index u
+    ci_mdvrptw = vrptw.clustered_clients[ci]
+    bi = route_starting_times[ci]
+    bu = calculate_starting_time_mdvrptw(mdvrptw, ci_mdvrptw, bi, city)
+    if bu > mdvrptw.time_windows[city][1]: #start time higher than last time of service
+        return False
+
+    cj = int(route[u])
+    cj_mdvrptw = vrptw.clustered_clients[cj]
+    bju = calculate_starting_time_mdvrptw(mdvrptw, city, bu, cj_mdvrptw)
+    bj = route_starting_times[cj]
+    PF = bju - bj
+
+    for temp_index in range(u, len(route)):
+        j_temp = int(route[temp_index])
+        if route_starting_times[j_temp] + PF > vrptw.time_windows[j_temp][1]:
+            return False
+
+    return True
 
 
 # Function: is_insertion_swap_viable_by_time_windows
