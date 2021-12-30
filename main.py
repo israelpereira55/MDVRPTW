0abffb9475d96a3ead74e5de5ca6930f1892c502
Israel <israelpereira55@gmail.com>
2021-12-30 16:08:42 -0300

    new main WIP

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ FLAGS = flags.FLAGS
 
 flags.DEFINE_string("instance", None, help="Path to the instance file")
 flags.DEFINE_boolean("toy", False, 'Run with a toy example instead of an instance.')
-flags.DEFINE_enum('cluster', 'kmeans', ['kmeans', 'urgencies', 'other'], 'Cluster method to be utilized.')
+#flags.DEFINE_enum('cluster', 'kmeans', ['kmeans', 'urgencies', 'other'], 'Cluster method to be utilized.')
 
 
 from vrp import MDVRPTW, VRPTW, solomon, plot
@@ -13,6 +13,14 @@ from vrp import MDVRPTW_Solution
 
 from vrp import clusterize, local_search, grasp
 from sklearn.cluster import AffinityPropagation, Birch, AgglomerativeClustering, KMeans
+
+from constants import Settings
+from settings import SoftwareSettings, ACOSettings, GRASPSettings, SolomonSettings
+
+
+def initial_population_grasp_aco(mdvrptw, clustered_clients, m, base_alpha=0.5):
+    solutions = initial_population(mdvrptw, clustered_clients, m, base_alpha)
+
 
 
 
@@ -25,22 +33,26 @@ def main(argv):
         print("Please describe the MDVRPTW instace file.\nAborting...")
         exit(1)
 
-    #random.seed(1)
+    software = SoftwareSettings('./settings/software.json')
+    grasp_settings = GRASPSettings('./settings/grasp.json')
+    aco_settings = ACOSettings('./settings/aco.json')
+    solomon_settings = SolomonSettings('./settings/solomon.json')
+    
     mdvrptw.print_instance()
-
+    random.seed(software.seed)
     # ============ DEFINING CLUSTERS =====================
     # ScikitLearn clusters
-    if FLAGS.cluster == 'kmeans':
+    if software.cluster == Settings.KMeans:
         initial_clusters = clusterize.get_initial_depots(mdvrptw)
         model = KMeans(n_clusters=mdvrptw.number_of_depots, init=initial_clusters, n_init=1) #fit and predict
         clustered_clients = clusterize.clusterize_by_scikitlearn(mdvrptw, model, show_warns=True)
 
-    elif FLAGS.cluster == 'urgencies':
+    elif software.cluster == Settings.Urgencies:
         # GIOSA, 2002
         # Article: New assignment algorithms for the multi-depot vehicle routing problem
         clustered_clients = clusterize.clusterize_by_urgencies(mdvrptw)
 
-    elif FLAGS.cluster == 'other': #TODO: Needs fixing
+    elif software.cluster == 'other': #TODO: Needs fixing
         model = Birch(threshold=0.01, n_clusters=mdvrptw.number_of_depots) #fit and predict
         #model = AgglomerativeClustering(n_clusters=mdvrptw.number_of_depots) #fit_predict
 
@@ -51,24 +63,37 @@ def main(argv):
     #plot.plot_clusterization_with_line(clustered_clients, mdvrptw.coordinates, mdvrptw.depots)
     # =====================================================
 
+    #SOLUTION METHOD
+    '''
+    if software.solve_method == Settings.GRASP:
+        mdvrptw_solution = grasp.construct_solution_with_solomon(mdvrptw, clustered_clients, grasp_settings, solomon_settings)
+        mdvrptw_solution.print_solution()
+        plot.plot_mdvrptw_solution(mdvrptw_solution)
+    else: #ACO
+        exit(1)
 
-    #mdvrptw_solution = grasp.construct_solution_with_solomon(mdvrptw, clustered_clients, alpha=0.8, max_iterations=10)
-    #print('best', mdvrptw_solution.get_travel_distance())
-    #mdvrptw_solution.print_solution()
-    #plot.plot_mdvrptw_solution(mdvrptw_solution)
-    #exit(1)
+    exit(1)
+    '''
+
+
 
     best_value = float('inf')
     time_start = time.time()
     sum_value = 0
-    for i in range(0,10):
+    for i in range(10):
+        #print("seed", i)
         random.seed(i)
-        mdvrptw_solution = grasp.construct_solution_with_solomon(mdvrptw, clustered_clients, alpha=0.8, max_iterations=10)
+        mdvrptw_solution = grasp.construct_solution_with_solomon(mdvrptw, clustered_clients, grasp_settings, solomon_settings)
         sum_value += mdvrptw_solution.get_travel_distance()
         print(mdvrptw_solution.get_travel_distance())
 
         if mdvrptw_solution.get_travel_distance() < best_value:
             best_value = mdvrptw_solution.get_travel_distance()
+
+        if not mdvrptw_solution.is_feasible():
+            print("not feasible")
+            exit(1)
+
 
     print("media")
     print(sum_value/10)
@@ -76,29 +101,6 @@ def main(argv):
     print("Constructive time:", (time_end - time_start)/10)
     print("Best value", round(best_value,2))
     exit(1)
-
-    mdvrptw_solution = MDVRPTW_Solution(mdvrptw, clustered_clients)
-
-    time_start = time.time()
-    mdvrptw_solution.construct_solution_with_solomon(alpha1=0.5, alpha2=0.5, mu=1, lambdaa=1)
-    time_end = time.time()
-    print("Constructive time:", time_end - time_start)
-    #mdvrptw_solution.print_solution()
-    #plot.plot_mdvrptw_solution(mdvrptw_solution)
-    #print("construtive", mdvrptw_solution.get_travel_distance() )
-
-    #local_search.local_search(mdvrptw_solution)
-    
-    time_start = time.time()
-    local_search.vnd(mdvrptw_solution)
-    #local_search.local_search(mdvrptw_solution)
-    time_end = time.time()
-    print("time:", time_end - time_start)
-    mdvrptw_solution.print_solution()
-    #plot.plot_mdvrptw_solution(mdvrptw_solution)
-
-    #print("FINAL:", mdvrptw_solution.get_travel_distance())
-
 
 
 if __name__ == "__main__":
