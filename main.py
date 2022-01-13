646dc67df6d846b7c8a480f7a968e8d2059abf02
Israel <israelpereira55@gmail.com>
2022-01-13 15:10:59 -0300

    new main WIP

--- main.py
+++ main.py
@@ -2,26 +2,16 @@ import time, random
 
 from absl import app, flags
 FLAGS = flags.FLAGS
-
 flags.DEFINE_string("instance", None, help="Path to the instance file")
 flags.DEFINE_boolean("toy", False, 'Run with a toy example instead of an instance.')
-#flags.DEFINE_enum('cluster', 'kmeans', ['kmeans', 'urgencies', 'other'], 'Cluster method to be utilized.')
 
-
-from vrp import MDVRPTW, VRPTW, solomon, plot
-from vrp import MDVRPTW_Solution
-
-from vrp import clusterize, local_search, grasp
 from sklearn.cluster import AffinityPropagation, Birch, AgglomerativeClustering, KMeans
 
 from constants import Settings
+from vrp import MDVRPTW, MDVRPTW_Solution, VRPTW 
+from vrp import solomon, plot, clusterize, local_search, grasp
 from settings import SoftwareSettings, ACOSettings, GRASPSettings, SolomonSettings
-
-
-def initial_population_grasp_aco(mdvrptw, clustered_clients, m, base_alpha=0.5):
-    solutions = initial_population(mdvrptw, clustered_clients, m, base_alpha)
-
-
+from aco import ACO
 
 
 def main(argv):
@@ -38,7 +28,7 @@ def main(argv):
     aco_settings = ACOSettings('./settings/aco.json')
     solomon_settings = SolomonSettings('./settings/solomon.json')
     
-    mdvrptw.print_instance()
+    #mdvrptw.print_instance()
     random.seed(software.seed)
     # ============ DEFINING CLUSTERS =====================
     # ScikitLearn clusters
@@ -59,49 +49,64 @@ def main(argv):
         clustered_clients = clusterize.clusterize_by_scikitlearn_ordering_depots(mdvrptw, model, show_warns=True)
         print(clustered_clients)
 
+    #plot.plot_instance(mdvrptw, plot_ids=False)
     #plot.plot_clusterization(clustered_clients, mdvrptw.coordinates, mdvrptw.depots)
-    #plot.plot_clusterization_with_line(clustered_clients, mdvrptw.coordinates, mdvrptw.depots)
+    #plot.plot_clusterization_with_line(mdvrptw, clustered_clients)
+    #exit(1)
     # =====================================================
 
     #SOLUTION METHOD
-    '''
     if software.solve_method == Settings.GRASP:
-        mdvrptw_solution = grasp.construct_solution_with_solomon(mdvrptw, clustered_clients, grasp_settings, solomon_settings)
-        mdvrptw_solution.print_solution()
-        plot.plot_mdvrptw_solution(mdvrptw_solution)
-    else: #ACO
+        best_solution = grasp.construct_solution_with_solomon(mdvrptw, clustered_clients, grasp_settings, solomon_settings, print_progress=True)
+        best_solution.print_solution()
+        plot.plot_mdvrptw_solution(best_solution)
+
+
+    elif software.solve_method == Settings.ACO_AS:
+        number_of_vertices = mdvrptw.number_of_clients + mdvrptw.number_of_depots
+        aco = ACO(number_of_vertices, aco_settings)
+        solutions = grasp.initial_population(mdvrptw, clustered_clients, grasp_settings, solomon_settings)
+        aco.inject_initial_population(solutions)
+        aco.run_AS(mdvrptw, clustered_clients, print_progress=True)
+        best_solution = aco.best_ant.mdvrptw_solution
+
+
+    elif software.solve_method == Settings.ACO_ACS:
+        number_of_vertices = mdvrptw.number_of_clients + mdvrptw.number_of_depots
+        aco = ACO(number_of_vertices, aco_settings)
+        solutions = grasp.initial_population(mdvrptw, clustered_clients, grasp_settings, solomon_settings)
+        aco.inject_initial_population(solutions)
+        aco.run_ACS(mdvrptw, clustered_clients, print_progress=True)
+        best_solution = aco.best_ant.mdvrptw_solution
+
+
+    else: #IPGRASP
+        time_start = time.time()
+        solutions = grasp.initial_population(mdvrptw, clustered_clients, grasp_settings, solomon_settings)
+        time_end = time.time()
+
+        best_cost = float('inf')
+        for i, solution in enumerate(solutions):
+            cost = solution.get_travel_distance()
+            if cost < best_cost:
+                best_i = i
+                best_cost = cost
+                best_solution = solution
+
+        alpha = grasp_settings.base_alpha + (best_i/(grasp_settings.m-1) * (1-grasp_settings.base_alpha))
+        print(f'[IPGRASP]: Best solution {best_cost} alpha {alpha}')
+        print("Time avg:", (time_end - time_start)/aco_settings.m)
+
+
+    #Test Solution: TEMPORARY
+    best_solution.check_clients_solution()
+    if round(best_solution.get_travel_distance(), 2) != round(best_solution.recalculate_travel_distance(), 2):
+        print("bad")
         exit(1)
 
-    exit(1)
-    '''
-
-
-
-    best_value = float('inf')
-    time_start = time.time()
-    sum_value = 0
-    for i in range(10):
-        #print("seed", i)
-        random.seed(i)
-        mdvrptw_solution = grasp.construct_solution_with_solomon(mdvrptw, clustered_clients, grasp_settings, solomon_settings)
-        sum_value += mdvrptw_solution.get_travel_distance()
-        print(mdvrptw_solution.get_travel_distance())
-
-        if mdvrptw_solution.get_travel_distance() < best_value:
-            best_value = mdvrptw_solution.get_travel_distance()
-
-        if not mdvrptw_solution.is_feasible():
-            print("not feasible")
-            exit(1)
-
-
-    print("media")
-    print(sum_value/10)
-    time_end = time.time()
-    print("Constructive time:", (time_end - time_start)/10)
-    print("Best value", round(best_value,2))
-    exit(1)
-
+    if not best_solution.is_feasible():
+        print('bad2')
+        exit(1)
 
 if __name__ == "__main__":
     app.run(main)
