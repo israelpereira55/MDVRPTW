46863ca54125a80a6bdb7c4c9c23837d45efcfd4
Israel <israelpereira55@gmail.com>
2022-01-13 15:08:58 -0300

    grasp development

--- vrp/grasp.py
+++ vrp/grasp.py
@@ -1,27 +1,18 @@
+import time
 import numpy as np
 
 import constants
 from constants import Settings
 from vrp import VRPTW_Solution, common, solomon, MDVRPTW_Solution, local_search
 
-# Initial population by GRASP - IPGRASP
-# (m)  number of solutions
-def initial_population(mdvrptw, clustered_clients, m, base_alpha=0.5):
-    solutions = []
-    for i in range(m):
-        alpha = base_alpha + (i/(m-1) * (1-base_alpha))
-        print("alpha", alpha)
 
-        solutions.append(construct_solution_with_solomon(mdvrptw, clustered_clients, alpha=alpha, max_iterations=1))
-
-    return solutions
-
-
-def construct_solution_with_solomon(mdvrptw, clustered_clients, grasp_settings, solomon_settings):
+def construct_solution_with_solomon(mdvrptw, clustered_clients, grasp_settings, solomon_settings, print_progress=False):
     mdvrptw_best_solution = None
     cost = float('inf')
 
     failed_attempts=0
+    if print_progress: time_start = time.time()
+    
     for i in range(grasp_settings.max_iterations):
         mdvrptw_solution = grasp_mdvrptw(mdvrptw, clustered_clients, grasp_settings.alpha, solomon_settings)
         
@@ -30,18 +21,8 @@ def construct_solution_with_solomon(mdvrptw, clustered_clients, grasp_settings, 
 
             failed_attempts += 1
             if failed_attempts > grasp_settings.number_of_attempts:
-                print("All attempts got an infeasible solution. Check parameters.")
+                print(f"[GRASP]: Iteration={i}. Number of infeasible generated solutions surpassed the maximum allowed: {grasp_settings.number_of_attempts}. Aborting...")
                 exit(1)
-
-        #Check solution
-        #====
-        mdvrptw_solution.check_clients_solution()
-        if not mdvrptw_solution.is_feasible():
-            mdvrptw_solution.print_solution()
-            print("infeasible???")
-            exit(1)
-        #==== TODO: REMOVE
-
 
         if grasp_settings.local_search == Settings.VND:
             local_search.vnd(mdvrptw_solution)
@@ -52,12 +33,11 @@ def construct_solution_with_solomon(mdvrptw, clustered_clients, grasp_settings, 
         if new_cost < cost:
             mdvrptw_best_solution = mdvrptw_solution
             cost = new_cost
+            if print_progress:
+                time_end = time.time()
+                print(f"[GRASP]: Improved solution {round(cost,2)} (iteration={i}, time={round(time_end-time_start,2)})")
 
-        if round(mdvrptw_solution.recalculate_travel_distance(),2) != round(mdvrptw_solution.get_travel_distance(),2):
-            print("problem")
-            print(round(mdvrptw_solution.recalculate_travel_distance(),2), round(mdvrptw_solution.get_travel_distance(),2))
-            exit(1)
-
+    if print_progress: print(f'[GRASP]: Number of infeasible generated solutions={failed_attempts}.')
     return mdvrptw_best_solution
 
 
@@ -68,3 +48,21 @@ def grasp_mdvrptw(mdvrptw, clustered_clients, alpha, solomon_settings):
         mdvrptw_solution.vrptw_solutions.append(vrptw_solution)
 
     return mdvrptw_solution
+
+
+# Initial population by GRASP - IPGRASP
+# (m)  number of solutions
+def initial_population(mdvrptw, clustered_clients, grasp_settings, solomon_settings):
+    solutions = []
+    m, base_alpha = grasp_settings.m, grasp_settings.base_alpha
+
+    initial_alpha = grasp_settings.alpha
+    for i in range(m):
+        alpha = base_alpha + (i/(m-1) * (1-base_alpha))
+
+        grasp_settings.alpha = alpha
+        solutions.append(construct_solution_with_solomon(mdvrptw, clustered_clients, grasp_settings, solomon_settings))
+        print("alpha", alpha, solutions[i].get_travel_distance())
+
+    grasp_settings.alpha = initial_alpha
+    return solutions
