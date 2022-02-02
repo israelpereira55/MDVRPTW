62ec6f58323b9b6df853b90a4c890e7ab4532170
Israel <israelpereira55@gmail.com>
2022-02-02 19:25:56 -0300

    added MDVRP

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from absl import app, flags
 FLAGS = flags.FLAGS
 flags.DEFINE_string("instance", None, help="Path to the instance file")
 flags.DEFINE_boolean("toy", False, 'Run with a toy example instead of an instance.')
+flags.DEFINE_boolean("MDVRP", False, 'Run with a toy example instead of an instance.')
 
 from sklearn.cluster import AffinityPropagation, Birch, AgglomerativeClustering, KMeans
 
@@ -18,6 +19,9 @@ def main(argv):
 
     mdvrptw = None
     if FLAGS.instance is not None:
+        if FLAGS.MDVRP:
+            mdvrptw = MDVRPTW(FLAGS.instance, MDVRP=True) #depot
+        else:
             mdvrptw = MDVRPTW(FLAGS.instance) #depot
     else:
         print("Please describe the MDVRPTW instace file.\nAborting...")
@@ -48,16 +52,22 @@ def main(argv):
 
         clustered_clients = clusterize.clusterize_by_scikitlearn_ordering_depots(mdvrptw, model, show_warns=True)
         print(clustered_clients)
-
     #plot.plot_instance(mdvrptw, plot_ids=False)
     #plot.plot_clusterization(clustered_clients, mdvrptw.coordinates, mdvrptw.depots)
     #plot.plot_clusterization_with_line(mdvrptw, clustered_clients)
     #exit(1)
     # =====================================================
+    mdvrptw.print_instance()
 
     #SOLUTION METHOD
     if software.solve_method == Settings.GRASP:
         best_solution = grasp.construct_solution_with_solomon(mdvrptw, clustered_clients, grasp_settings, solomon_settings, print_progress=True)
+        best_solution.print_solution()
+        plot.plot_mdvrptw_solution(best_solution)
+
+
+    elif software.solve_method == Settings.GRASP_REACTIVE:
+        best_solution = grasp.reactive_grasp(mdvrptw, clustered_clients, grasp_settings, solomon_settings)
         best_solution.print_solution()
         plot.plot_mdvrptw_solution(best_solution)
 
@@ -74,8 +84,8 @@ def main(argv):
     elif software.solve_method == Settings.ACO_ACS:
         number_of_vertices = mdvrptw.number_of_clients + mdvrptw.number_of_depots
         aco = ACO(number_of_vertices, aco_settings)
-        solutions = grasp.initial_population(mdvrptw, clustered_clients, grasp_settings, solomon_settings)
-        aco.inject_initial_population(solutions)
+        #solutions = grasp.initial_population(mdvrptw, clustered_clients, grasp_settings, solomon_settings)
+        #aco.inject_initial_population(solutions)
         aco.run_ACS(mdvrptw, clustered_clients, print_progress=True)
         best_solution = aco.best_ant.mdvrptw_solution
 
