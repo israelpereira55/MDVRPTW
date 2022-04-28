5ff63ca04a67e96008a77b3c14efe017c4906b2e
Israel Pereira <israel.pereira@motora.ai>
2022-04-28 11:34:42 -0300

    new main WIP

--- main.py
+++ main.py
@@ -16,6 +16,12 @@ from aco import ACO
 
 
 def main(argv):
+    # ============ READING SETTINGS ======================
+    software = SoftwareSettings('./settings/software.json', print_settings=True)
+    grasp_settings = GRASPSettings('./settings/grasp.json', print_settings=True)
+    aco_settings = ACOSettings('./settings/aco.json', print_settings=True)
+    solomon_settings = SolomonSettings('./settings/solomon.json', print_settings=True)
+    # =====================================================
 
     mdvrptw = None
     if FLAGS.instance is not None:
@@ -27,13 +33,10 @@ def main(argv):
         print("Please describe the MDVRPTW instace file.\nAborting...")
         exit(1)
 
-    software = SoftwareSettings('./settings/software.json', print_settings=True)
-    grasp_settings = GRASPSettings('./settings/grasp.json', print_settings=True)
-    aco_settings = ACOSettings('./settings/aco.json', print_settings=True)
-    solomon_settings = SolomonSettings('./settings/solomon.json', print_settings=True)
-    
     #mdvrptw.print_instance()
     random.seed(software.seed)
+
+
     # ============ DEFINING CLUSTERS =====================
     # ScikitLearn clusters
     if software.cluster == Settings.KMeans:
@@ -46,32 +49,41 @@ def main(argv):
         # Article: New assignment algorithms for the multi-depot vehicle routing problem
         clustered_clients = clusterize.clusterize_by_urgencies(mdvrptw)
 
+    elif software.cluster == Settings.ThreeCriteriaClustering:
+        # GIOSA, 2002
+        # Article: New assignment algorithms for the multi-depot vehicle routing problem
+        clustered_clients = clusterize.three_criteria_clustering(mdvrptw, show_test=False)
+
     elif software.cluster == 'other': #TODO: Needs fixing
         model = Birch(threshold=0.01, n_clusters=mdvrptw.number_of_depots) #fit and predict
         #model = AgglomerativeClustering(n_clusters=mdvrptw.number_of_depots) #fit_predict
 
         clustered_clients = clusterize.clusterize_by_scikitlearn_ordering_depots(mdvrptw, model, show_warns=True)
         print(clustered_clients)
+
     #plot.plot_instance(mdvrptw, plot_ids=False)
     #plot.plot_clusterization(clustered_clients, mdvrptw.coordinates, mdvrptw.depots)
     #plot.plot_clusterization_with_line(mdvrptw, clustered_clients)
-    #plt.savefig('instance-mdvrp.png', bbox_inches='tight')
     # =====================================================
+
     #mdvrptw.print_instance()
-    best_solution = None
 
     #mdvrptw_solution = MDVRPTW_Solution(mdvrptw, clustered_clients)
     #mdvrptw_solution.construct_solution_with_solomon(solomon_settings)
     #print("Construtive", mdvrptw_solution.get_travel_distance())
 
+    best_solution = None
+
     #SOLUTION METHOD
+
+    # ============ GRASP METAHEURISTIC ===================
     if software.solve_method == Settings.GRASP:
         best_solution = grasp.construct_solution_with_solomon(mdvrptw, clustered_clients, grasp_settings, solomon_settings, print_progress=True)
 
     elif software.solve_method == Settings.GRASP_REACTIVE:
-        best_solution = grasp.reactive_grasp(mdvrptw, clustered_clients, grasp_settings, solomon_settings)
+        best_solution = grasp.reactive_grasp(mdvrptw, clustered_clients, grasp_settings, solomon_settings, print_progress=True)
 
-
+    # =========== ANT COLONY METAHEURISTIC ===============
     elif software.solve_method == Settings.ACO_AS:
         number_of_vertices = mdvrptw.number_of_clients + mdvrptw.number_of_depots
         aco = ACO(number_of_vertices, aco_settings)
@@ -88,7 +100,7 @@ def main(argv):
         aco.run_ACS(mdvrptw, clustered_clients, print_progress=True)
         best_solution = aco.best_ant.mdvrptw_solution
 
-
+    # =========== RANDOM AND LOCAL SEARCH ================
     elif software.solve_method == Settings.RANDOM_VND:
         sum_solutions = 0
         best_cost = float('inf')
@@ -112,7 +124,7 @@ def main(argv):
 
         print(f'[RANDOM VND]: Average cost of solutions={round(sum_solutions/10000, 2)}.')
 
-
+    # TEMP
     else: #IPGRASP
         time_start = time.time()
         solutions = grasp.initial_population(mdvrptw, clustered_clients, grasp_settings, solomon_settings)
@@ -129,21 +141,21 @@ def main(argv):
         alpha = grasp_settings.base_alpha + (best_i/(grasp_settings.m-1) * (1-grasp_settings.base_alpha))
         print(f'[IPGRASP]: Best solution {best_cost} alpha {alpha}')
         print("Time avg:", (time_end - time_start)/aco_settings.m)
-
-
+    # =====================================================
 
     best_solution.print_solution()
     plot.plot_mdvrptw_solution(best_solution)
 
     #Test Solution: TEMPORARY
     best_solution.check_clients_solution()
-    if round(best_solution.get_travel_distance(), 2) != round(best_solution.recalculate_travel_distance(), 2):
+    if round(best_solution.get_travel_distance(), 4) != round(best_solution.recalculate_travel_distance(), 4):
         print("bad")
         exit(1)
 
     if not best_solution.is_feasible():
         print('bad2')
         exit(1)
+
 
 if __name__ == "__main__":
     app.run(main)
